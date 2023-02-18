d8841930ea095159b0128d994b8e4a5f137d74bb
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-02-18 11:11:48 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/Underactuated.jl
@@ -0,0 +1,5 @@
+module Underactuated
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Underactuated
+using Test
+
+@testset "Underactuated.jl" begin
+    # Write your tests here.
+end
